52fd1bbb1a192053fc38053f629ca702abd63da7
Rehan1032 <rehanbate7@gmail.com>
2022-07-22 20:33:16 +0530

    first commit

---
+++ apidocuments.md
@@ -0,0 +1,30 @@
+
+page1
+
+///// Category/////
+
+http://localhost:9875/category
+
+
+
+////// Subcategory///////
+
+http://localhost:9875/Subcategory
+
+
+
+////// Product//////////
+
+http://localhost:9875/product
+
+
+////// carousel///////
+http://localhost:9875/carousel
+
+
+page 2
+
+//////  snack store ///////
+
+http://localhost:9875/snacks
+
---
+++ app.js
@@ -0,0 +1,67 @@
+let express = require('express');
+let app = express();
+let dotenv = require('dotenv');
+dotenv.config();
+let port = process.env.PORT || 7800;
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+let MongoUrl= process.env.MongoLiveUrl;
+let db;
+
+app.get('/',(req,res)=>{
+    res.send('Hii From express')
+})
+
+app.get('/category',(req,res)=>{
+    db.collection('category').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+   
+})
+
+app.get('/Subcategory',(req,res)=>{
+    db.collection('Subcategory').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+   
+})
+
+app.get('/product',(req,res)=>{
+    db.collection('product').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+   
+})
+
+app.get('/Carousel',(req,res)=>{
+    db.collection('Carousel').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+   
+})
+
+app.get('/snacks',(req,res)=>{
+    db.collection('Carousel').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+   
+})
+
+
+// connection with db
+MongoClient.connect(MongoUrl,(err,client) =>{
+    if(err) console.log(`Error While Connecting`);
+    db = client.db('MyProject2');
+    app.listen(port,() => {
+        console.log(`listening on port ${port}`);
+    })
+})
+
+
+
+
